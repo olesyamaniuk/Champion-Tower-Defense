b748297aac20f3b16fe7152aed0c641a722ecedf
olesyamaniuk <maniukolesya222@gmail.com>
2024-11-13 14:59:03 +0200

    1

--- js/slick.js
+++ js/slick.js
@@ -1,36 +1,4 @@
 $(function () {
-  $('.gallery__list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: true,
-    centerMode: true,
-    cssEase: 'linear',
-    // nextArrow: '.reviews__button--right',
-  });
-});
-
-$(function () {
-  $('.advantages__list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: true,
-    centerMode: true,
-    cssEase: 'linear',
-    // nextArrow: '.reviews__button--right',
-  });
-});
-
-$(function () {
   $('.play__list').slick({
     dots: true,
     infinite: true,
@@ -40,7 +8,7 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-   
+    dots: true,
     centerMode: true,
     cssEase: 'linear',
     nextArrow: '.play__button',
